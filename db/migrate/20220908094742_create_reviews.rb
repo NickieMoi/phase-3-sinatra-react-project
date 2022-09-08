a127fca6f671af507c4733ccaf9b8617f0a42de8
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-08 13:10:50 +0300

    modifies the migratons in reviews

---
+++ db/migrate/20220908094742_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change 
+    create_table :review do |t|
+    t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
