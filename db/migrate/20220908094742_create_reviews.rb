64ecb9150e43e54c4548e2e833ad0afae36871b1
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-09 16:31:27 +0300

    migrates the review model

--- db/migrate/20220908094742_create_reviews.rb
+++ db/migrate/20220908094742_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
-  def change 
+  def change
     create_table :reviews do |t|
-    t.string :name
-      t.string :description
+      t.string :body
+      t.integer :hotel_id
       t.timestamps
     end
   end
